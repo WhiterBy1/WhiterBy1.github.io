75e9760c325b4f13b00a48be6da22286dc1879d5
WhiterBy <80159350+WhiterBy1@users.noreply.github.com>
2022-06-10 19:25:41 -0500

    initial commit

--- WB1/js/MenuCode.js
+++ WB1/js/MenuCode.js
@@ -1,6 +1,5 @@
 const btnmenu = document.querySelector("#btnMenu")
 const menu = document.querySelector("#menu")
-const whts = document.querySelector("#SM")
 
 btnMenu.addEventListener("click", ()=>{
         menu.classList.toggle("spread")
